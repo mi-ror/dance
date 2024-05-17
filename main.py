8f5eb9ed0da231008759ae3da5a92461f9083861
Denis Zverev <mi-ror@github.com>
2024-05-17 19:31:06 +0300

    logging and parse args

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
+import logging
 import deck
 from ai_realization import *
 from table import Table
 import statistics
+import argparse
 
 
-def play_game():
+l = logging.getLogger(__name__)
+
+
+def play_game(player_nums: int):
     m_deck = deck.get_monsters_shuffled()
-    # [print(m) for m in m_deck]
+    [l.debug(m) for m in m_deck]
     c_deck = deck.get_cards_shuffled()
-    # [print(m) for m in c_deck]
-    # print('DEAL CARDS...')
-    t = Table(4, m_deck, c_deck, AI_T800())
-    # print(t)
-    # print('CABESEO...')
+    [l.debug(m) for m in c_deck]
+    l.debug('DEAL CARDS...')
+    t = Table(player_nums, m_deck, c_deck, AI_T800())
+    l.debug(t)
+    l.debug('CABESEO...')
     t.do_cabeseo()
-    # print(t)
-    # print('PLAYING GAME...')
+    l.debug(t)
+    l.debug('PLAYING GAME...')
     while t.do_turn():
         pass
-    # print(t)
+    l.debug(t)
     statistics.consider_game(t)
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Simulate card game 'Dance with Monsters'.")
+    parser.add_argument('--player-nums', type=int, action='store', default=6, help='count of players')
+    args = parser.parse_args()
+    l.info(args.player_nums)
+
     deck.load_monster_deck()
     deck.load_card_deck()
-    for _ in range(3 * 1000):
-        play_game()
+
+    for _ in range(3 * 1):
+        play_game(args.player_nums)
+
     print('###STATISTICS###')
     statistics.print_statistics()
 
